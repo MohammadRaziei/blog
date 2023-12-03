6220424f69c3ac39a8db9e509a944f5ba3d608ad
mohammadraziei <mohammadraziei1375@gmail.com>
2023-12-04 00:26:59 +0330

    rename all fork-name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://mohammadraziei.github.io/blog',
+  siteRepo: 'https://github.com/mohammadraziei/blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
