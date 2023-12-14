d5ee50783d0285c3f5f2153d8a57be46bb040bb2
mohammadraziei <mohammadraziei1375@gmail.com>
2023-12-14 22:09:22 +0330

    fix link

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,12 +3,13 @@ const projectsData = [
     title: 'A Search Engine',
     description: `Zarebin is an Iranian app aiming to improve user experience with web content and Persian searches. It offers intelligent answers and topic-specific search engines in various areas like music, video, shopping, and news. Zarebin interacts with other search engines and advocates for transparent regulations to protect users in the online space. The team comprises young and elite individuals from reputable universities, with a vision to facilitate access to web services for all Persian speakers. Their mission is to provide a unified platform of intelligent search engines for centralized access to Persian virtual information and services.`,
     imgSrc: '/static/images/zarebin.png',
-    href: 'https://www.zarebin.ir/about',
+    href: 'https://www.zarebin.ir/about-us',
   },
   {
     title: 'liburlparser',
     description: `liburlparser is a versatile URL parsing library offering high-performance domain extraction in C++ with seamless Python bindings. It features an intuitive interface across both languages, supports automatic updates to the public suffix list, and excels in speed, outperforming other libraries in benchmarks. Ideal for developers needing robust URL analysis in C++ and Python environments.`,
-    imgSrc: 'https://raw.githubusercontent.com/MohammadRaziei/liburlparser/master/docs/images/logo/liburlparser-logo-1.svg',
+    imgSrc:
+      'https://raw.githubusercontent.com/MohammadRaziei/liburlparser/master/docs/images/logo/liburlparser-logo-1.svg',
     href: '/blog/projects/liburlparser',
   },
 ]
