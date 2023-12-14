b67a04f85d78cc094ffe23e41ed0118a0e6047f8
mohammadraziei <mohammadraziei1375@gmail.com>
2023-12-14 19:47:16 +0330

    adding projects to this blog

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
     title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    description: `Zarebin is an Iranian app aiming to improve user experience with web content and Persian searches. It offers intelligent answers and topic-specific search engines in various areas like music, video, shopping, and news. Zarebin interacts with other search engines and advocates for transparent regulations to protect users in the online space. The team comprises young and elite individuals from reputable universities, with a vision to facilitate access to web services for all Persian speakers. Their mission is to provide a unified platform of intelligent search engines for centralized access to Persian virtual information and services.`,
+    imgSrc: '/static/images/zarebin.png',
+    href: 'https://www.zarebin.ir/about',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'liburlparser',
+    description: `liburlparser is a versatile URL parsing library offering high-performance domain extraction in C++ with seamless Python bindings. It features an intuitive interface across both languages, supports automatic updates to the public suffix list, and excels in speed, outperforming other libraries in benchmarks. Ideal for developers needing robust URL analysis in C++ and Python environments.`,
+    imgSrc: 'https://raw.githubusercontent.com/MohammadRaziei/liburlparser/master/docs/images/logo/liburlparser-logo-1.svg',
+    href: '/blog/projects/liburlparser',
   },
 ]
 
