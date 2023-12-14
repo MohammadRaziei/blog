9d634940b898c099747eb9c1897d95545e07fde5
mohammadraziei <mohammadraziei1375@gmail.com>
2023-12-14 22:07:24 +0330

    changing the project sentence

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,9 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            {/*Showcase your projects with a hero image (16 x 9)*/}
+            Discover Mohammad Raziei’s range of projects, spanning technology, social initiatives,
+            and more in one place.
           </p>
         </div>
         <div className="container py-12">
